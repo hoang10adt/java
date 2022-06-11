7d9f97c8fd709a6b2be8df4aad66fc30f3521b8a
hoang10adt <85113756+hoang10adt@users.noreply.github.com>
2022-06-12 00:41:04 +0700

    Create Test_findGV.java

---
+++ Test_findGV.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GGV;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author TrungKien
+ */
+public class Test_findGV {
+    public static void dogetGV(){
+        XLGV xl = new XLGV();
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap don vi cong tac: ");
+        String dv = sc.next();
+        System.out.println("Nhap so cong trinh: ");
+        int ct = sc.nextInt();
+        xl.getGV(dv, ct);
+    }
+    
+    public static void main(String[] args){
+        dogetGV();
+    }
+}
